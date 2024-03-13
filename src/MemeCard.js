b08e5e5381992667f06caf1d1f6581c3f9f91509
krishanmurari <krishanmurari0596@gmail.com>
2024-03-13 12:19:54 +0530

    meme image fixed

--- src/MemeCard.js
+++ src/MemeCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './index.css';
 const MemeCard = ({ data }) => {
-  const { memeUrl, title } = data;
-  console.log("data", data);
+  const { url, title } = data;
+  console.log("data", data, url);
   return (
     <div className="p-5 m-5 border border-black  rounded-lg">
-      <img className="h-64 w-64" alt="meme" src={memeUrl} />
+      <img className="h-64 w-64" alt="meme" src={url} />
       <p>{title}</p>
     </div>
   );
